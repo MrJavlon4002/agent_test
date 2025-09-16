9aad5f23a813a094b2c86c392e3d1b1835224c33
MrJavlon4002 <valiyevjavlon001@gmail.com>
2025-09-16 15:04:56 +0500

    midlware access removed

--- back/app.py
+++ back/app.py
@@ -21,17 +21,8 @@ SESSION_KEY = lambda sid: f"session:{sid}"
 
 app = FastAPI(title="Sello AI backend")
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:4173",
-    "http://localhost:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
